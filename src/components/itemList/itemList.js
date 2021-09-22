64e3967bb6f956b76e1adc7855d6dedaac7ff74a
Anatoliy Klimov <AnatoliyKlimov@Gmail.com>
2021-09-23 02:10:19 +0300

    pages, patterns + some refactors

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,68 +1,47 @@
 import React, { Component } from "react";
 import "./itemList.css";
-import gotService from "../../services/gotService";
-import ErrorMessage from "../errorMessage";
 import Spinner from "../spinner";
 
 export default class ItemList extends Component {
-  gotService = new gotService();
-
   state = {
-    charList: null,
-    error: false,
+    itemList: null,
   };
   componentDidMount() {
-    this.gotService
-      .getAllCharacters()
-      .then((charList) => {
-        this.setState({
-          charList,
-          error: false,
-        });
-      })
-      .catch(() => {
-        this.onError();
+    const { getData } = this.props;
+
+    getData().then((itemList) => {
+      this.setState({
+        itemList,
       });
-  }
-  componentDidCatch() {
-    this.setState({
-      charList: null,
-      error: true,
     });
   }
-  onError(status) {
-    this.setState({
-      charList: null,
-      error: true,
-    });
-  }
+
   renderItems(arr) {
     return arr.map((item) => {
-      const { id, name } = item;
+      const { id } = item;
+
+      const label = this.props.renderItem(item);
+
       return (
         <li
           key={id}
           className="list-group-item"
-          onClick={() => this.props.onCharSelected(id)}
+          onClick={() => this.props.onItemSelected(id)}
         >
-          {name}
+          {label}
         </li>
       );
     });
   }
 
   render() {
-    const { charList, error } = this.state;
+    const { itemList } = this.state;
 
-    if (error) {
-      return <ErrorMessage />;
-    }
-
-    if (!charList) {
+    if (!itemList) {
       return <Spinner />;
     }
 
-    const items = this.renderItems(charList);
+    const items = this.renderItems(itemList);
 
     return <ul className="item-list list-group">{items}</ul>;
   }
